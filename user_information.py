120f9aacd6b36dc360f96afcb20e23c9d4938cd8
nazirova <nazirovamasuma@gmail.com>
2024-01-29 18:18:38 +0500

    fayl qo'shildi
    Sodda dastur yuklandi

---
+++ user_information.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 27 16:31:50 2024
+
+@author: Nazirova
+"""
+ism=input('Ismingiz>>>')
+familiya=input('Familiyangiz>>>')
+yosh=input('Yoshingiz>>>')
+print(f"{ism.title()} {familiya.title()} {yosh} yosh.")
